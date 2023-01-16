a4d2faba227640cd7add0176e9d6caf8e3830bbd
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-01-15 21:23:12 -0800

    Add Sidebar when loggedIn

--- e-learning/src/App.js
+++ e-learning/src/App.js
@@ -1,14 +1,38 @@
 import Navbar from "./Navbar";
 import SubjectOverview from "./SubjectOverview";
-import "./styles/App.scss";
 import Concept from "./Concept";
 import Practice from "./Practice";
+import "./styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Practice />
+      <Navbar loggedIn />
+      <div className="loggedIn-layout">
+        <div className="Sidebar">
+          <ul className="Sidebar-list">
+            <li className="Sidebar-item active">
+              <ion-icon name="grid-outline"></ion-icon>
+              <a href="dashboard" className="link">
+                Dashboard
+              </a>
+            </li>
+            <li className="Sidebar-item">
+              <ion-icon name="library-outline"></ion-icon>
+              <a href="dashboard" className="link">
+                Subjects
+              </a>
+            </li>
+            <li className="Sidebar-item">
+              <ion-icon name="book-outline"></ion-icon>
+              <a href="dashboard" className="link">
+                Homework
+              </a>
+            </li>
+          </ul>
+        </div>
+        <Practice />
+      </div>
     </div>
   );
 }
