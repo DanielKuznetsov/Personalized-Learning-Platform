81109e0723b391af1f07a1d6138adfaca6eb8178
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-01-16 10:17:18 -0800

    Adjust the height to stretch 100% vertically

--- e-learning/src/App.js
+++ e-learning/src/App.js
@@ -8,6 +8,8 @@ function App() {
   return (
     <div className="App">
       <Navbar loggedIn />
+      {/* NEED TO MOVE TO ITS OWN COMPONENT THE SIDEBAR */}
+      {/* NEED TO CREATE ITS OWN COMPONENT FOR THE SIDEBAR LIST */}
       <div className="loggedIn-layout">
         <div className="Sidebar">
           <ul className="Sidebar-list">
@@ -31,7 +33,7 @@ function App() {
             </li>
           </ul>
         </div>
-        <Practice />
+        <Concept />
       </div>
     </div>
   );
