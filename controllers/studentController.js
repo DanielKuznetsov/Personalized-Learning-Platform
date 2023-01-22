3efe315532bb401edbcfa83d2c2161ec49b44f80
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-01-21 21:27:49 -0800

    Add signup functionality

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,12 +1,46 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const Student = require("../models/studentModel");
 
-exports.signupStudent = (req, res) => {
-  res.status(200).json({
-    message: "Signup route",
+exports.signupStudent = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+
+    throw new Error("Fill out all fields!");
+  }
+
+  const studentExists = await Student.findOne({ email });
+
+  if (studentExists) {
+    res.status(400);
+
+    throw new Error("Student with this email already exists!");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const student = await Student.create({
+    name,
+    email,
+    password: hashedPassword,
   });
-};
+
+  if (student) {
+    res.status(201).json({
+      student,
+    });
+  } else {
+    res.status(400);
+
+    throw new Error("Invalid data!");
+  }
+});
 
 exports.loginStudent = (req, res) => {
   res.status(200).json({
@@ -17,5 +51,12 @@ exports.loginStudent = (req, res) => {
 exports.getMe = (req, res) => {
   res.status(200).json({
     message: "Student's data route",
+  });
+};
+
+// Generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "90d",
   });
 };
