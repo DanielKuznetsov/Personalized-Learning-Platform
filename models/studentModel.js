90ce54087d90ad0c3680b688267291c34be36346
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-01-21 20:46:03 -0800

    Redo the student model

---
+++ models/studentModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please tell us your name!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please tell us your email!"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email!"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please tell your password!"],
+      minlength: 8,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["student", "teacher", "admin"],
+      default: "student",
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+studentSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  // .find() outputs only the docs with active set to -> true
+  const students = this.find({ active: { $ne: false } });
+
+  next();
+});
+
+const Student = mongoose.model("Student", petSchema);
+
+module.exports = Student;
