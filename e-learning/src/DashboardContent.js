fcbbe4878539b38a62d146181692d2716e15e6e5
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-01-23 18:42:33 -0800

    Get the data change dynamically in DashboardContent, Table and Header

--- e-learning/src/DashboardContent.js
+++ e-learning/src/DashboardContent.js
@@ -1,11 +1,20 @@
 import Header from "./Header";
 import Table from "./Table";
 
-function DashboardContent() {
+function DashboardContent({ classActive }) {
+  // Find which element is active
+  const chapterName = Object.entries(classActive)
+    .filter((x) => x[1] === true)
+    .map((x) => x[0])[0];
+
+  // Later add an axios request to output correct data and pass it as a prop
+  // Data must include: title, subtitle, # of topics and problems, chapters, topics, and links
+  // This data needs to be stored in REDUX GLOBAL STATE!!!
+
   return (
     <div className="subject-content">
-      <Header />
-      <Table />
+      <Header chapterName={chapterName} />
+      <Table chapterName={chapterName} />
     </div>
   );
 }
--- e-learning/src/Header.js
+++ e-learning/src/Header.js
@@ -1,6 +1,6 @@
 import "./styles/Header.scss";
 
-function Header() {
+function Header({ chapterName }) {
   return (
     <header className="Header">
       <div className="Header-svg">
@@ -46,7 +46,7 @@ function Header() {
             <path d="m8.596 13.218c-.051 0-.103-.008-.155-.025l-3.939-1.283c-.263-.086-.406-.368-.321-.63s.367-.406.63-.321l3.939 1.283c.263.086.406.368.321.63-.068.212-.264.346-.475.346z" />
           </g>
         </svg>
-        <p className="Header-title">Calculus</p>
+        <p className="Header-title">{chapterName}</p>
       </div>
       <p className="Header-subtitle">
         There are <b>128 topics</b> available in a library with <b>9</b>{" "}
