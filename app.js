e95b8ede6fd5ff0423a873e1ba5b7a0088384cb2
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-04 15:23:15 -0800

    Update Server files

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./e-learning/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve("../", "e-learning", "build", "index.html"))
+    res.sendFile(path.resolve("../", "app", "e-learning", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("Not in production"));
