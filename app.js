fe9ec9384f641781ee65121dd2c66c72f52dd300
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-04 14:41:08 -0800

    Update Server files

--- app.js
+++ app.js
@@ -30,11 +30,11 @@ app.use("/api/students", require("./routes/studentRoutes"));
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./e-learning/build")));
+  app.use(express.static(path.join(__dirname, "../e-learning/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../e-learning/build/index.html")
+      path.resolve(__dirname, "../", "e-learning", "build", "index.html")
     )
   );
 } else {
