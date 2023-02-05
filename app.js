ba0ebd2c3430e95251c12f162c24a50a8c0b3598
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-04 21:24:28 -0800

    Update Server files

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false }));
