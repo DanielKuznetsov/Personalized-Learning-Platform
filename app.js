0b754e60509bca6ad01955021bfb0673910dad14
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-04 21:25:52 -0800

    Update Server files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
   next();
 });
 
