1857228f3b10faf865fb88813d79cbf002534461
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-04 20:25:14 -0800

    Update Server files

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:4000",
     credentials: true,
   })
 );
 
-app.options("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
+// app.options("*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Credentials", "true");
 
-  next()
-});
+//   next()
+// });
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
